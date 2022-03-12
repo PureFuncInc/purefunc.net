146f58ece63d50e16100fee0edac96bfbe665099
Nelson Liu <nelsliu9121@gmail.com>
2022-03-12 11:51:10 +0800

    feat: add vanilla-extract

---
+++ src/app.css.ts
@@ -0,0 +1,8 @@
+import { globalStyle } from '@vanilla-extract/css'
+
+globalStyle(
+  'html, body',
+  {
+    fontFamily: "'Open Sans', 'Helvetica Neue', sans-serif",
+  },
+)
--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,34 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
-};
+    prerender: {
+      default: true,
+    },
 
-export default config;
+    vite: {
+      define: {
+        'process.env.VITE_BUILD_TIME': JSON.stringify(new Date()
+          .toISOString()),
+      },
+      plugins: [ vanillaExtractPlugin() ],
+      ssr:
+        process.env.NODE_ENV === 'development'
+          ? {}
+          : {
+            noExternal: [ '@vanilla-extract/css', '@vanilla-extract/css/fileScope' ],
+          },
+    },
+  },
+}
+
+export default config
